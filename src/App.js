aea0009438922ee850120e76ad0d18161a2936db
K1ng-T0ast <kylejwhite@proton.me>
2023-04-17 17:15:15 -0700

    Feature map complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Components/Header';
 import Main from './Components/Main';
 import Footer from './Components/Footer';
+import './App.css';
 
 class App extends React.Component {
   render() {
